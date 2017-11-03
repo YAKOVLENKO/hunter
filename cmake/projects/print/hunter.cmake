b9f00d5a8af596f6ce2eb52f349fdd10e9a2eb74
YAKOVLENKO <svobodinayakovlenko@yandex.ru>
2017-11-03 08:36:20 +0300

    added print package

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/YAKOVLENKO/lab09/archive/v0.1.0.0.tar.gz"
+    SHA1
+    dd2c3fd32005e0201b000512218564451b90ae61
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
